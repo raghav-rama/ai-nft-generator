b0da3ff81f5661e723dfd6dd5839531eb3adab2f
raghav-rama <hackerer528@gmail.com>
2023-09-02 17:41:24 +0530

    fix: initialize iinstruction

--- programs/ai-nft-generator/src/lib.rs
+++ programs/ai-nft-generator/src/lib.rs
@@ -6,7 +6,7 @@ pub use model::*;
 pub mod actions;
 pub use actions::*;
 
-declare_id!("ATDso9o279ksvov6rAm4MoXQrxFPQX1q5qiX4yXQLnxh");
+declare_id!("9zFjUs3LhY39t7HbJWMBFt6VrBMktVzbvBXAjFo7V8yS");
 
 pub const PROGRAM_SEED: &[u8] = b"ai-nft-generator";
 
@@ -17,17 +17,12 @@ pub mod ai_nft_generator {
     use super::*;
 
     #[access_control(ctx.accounts.validate(&ctx, &params))]
-    pub fn initialize(ctx: Context<Initialize>, params: InitializeParams) -> Result<()> {
-        Ok(())
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        params: InitializeParams,
+    ) -> anchor_lang::Result<()> {
+        Initialize::actuate(&ctx, &params)
     }
-
-    // #[access_control(ctx.accounts.validate(&ctx, &params))]
-    // pub fn initialize(
-    //     ctx: Context<Initialize>,
-    //     params: InitializeParams,
-    // ) -> anchor_lang::Result<()> {
-    //     Initialize::actuate(&ctx, &params)
-    // }
 
     // #[access_control(ctx.accounts.validate(&ctx, &params))]
     // pub fn refresh_oracles(
@@ -36,21 +31,4 @@ pub mod ai_nft_generator {
     // ) -> anchor_lang::Result<()> {
     //     NftMetadata::actuate(&ctx, &params)
     // }
-}
-
-#[derive(Accounts)]
-#[instruction(params: InitializeParams)]
-pub struct Initialize<'info> {
-    pub oracle: AccountLoader<'info, MyOracleState>,
-}
-
-#[derive(Clone, AnchorDeserialize, AnchorSerialize)]
-pub struct InitializeParams {
-    pub n: u128,
-}
-
-impl Initialize<'_> {
-    pub fn validate(&self, _ctx: &Context<Self>, _params: &InitializeParams) -> Result<()> {
-        Ok(())
-    }
 }
