0d4f822003c828964124c51d798c63cae79457c5
raghav-rama <hackerer528@gmail.com>
2023-09-03 22:34:27 +0530

    feat: add generate_nft instruction

---
+++ programs/ai-nft-generator/src/actions/generate_nft.rs
@@ -0,0 +1,35 @@
+use crate::*;
+use switchboard_solana::FunctionAccountData;
+
+#[derive(Accounts)]
+pub struct GenerateNft<'info> {
+    #[account(
+        mut, 
+        seeds = [ORACLE_SEED],
+        bump = oracle.load()?.bump
+    )]
+    pub oracle: AccountLoader<'info, MyOracleState>,
+    #[account(
+        constraint = function.load()?.validate(&enclave_signer.to_account_info())? @ AiNftGenerateError::FunctionValidationFailed
+    )]
+    pub function: AccountLoader<'info, FunctionAccountData>,
+    pub enclave_signer: Signer<'info>
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct GenerateNftParams {
+    pub nft: NftMetadataBorsh,
+}
+
+
+impl GenerateNft<'_> {
+    pub fn validate(&self, _ctx: &Context<GenerateNft>, _params: &GenerateNftParams) -> Result<()> {
+        Ok(())
+    }
+    pub fn actuate(ctx: Context<Self>, params: GenerateNftParams) -> Result<()> {
+        let oracle = &mut ctx.accounts.oracle.load_mut()?;
+        msg!("saving nft metadata");
+        oracle.update_nft_metadata(params.nft)?;
+        Ok(())
+    }
+}
