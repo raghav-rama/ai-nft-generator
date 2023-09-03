a22b02939706111a0652274c64e959c616f66541
raghav-rama <hackerer528@gmail.com>
2023-09-03 10:40:34 +0530

    feat: update tests

--- tests/ai-nft-generator.ts
+++ tests/ai-nft-generator.ts
@@ -1,26 +1,158 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { PublicKey, Connection, Commitment } from "@solana/web3.js";
 import { AiNftGenerator } from "../target/types/ai_nft_generator";
+import wallet from "../wallet.json";
+import {
+  FunctionRequestSetConfigParams,
+  FunctionRequestTriggerParams,
+  FunctionRequestAccountInitParams,
+  FunctionTriggerParams,
+  FunctionRequestAccount,
+  SendTransactionObjectOptions,
+  SwitchboardProgram,
+  FunctionAccount,
+  attestationTypes,
+  AttestationQueueAccount,
+  AttestationProgramStateAccount,
+} from "@switchboard-xyz/solana.js";
+import { parseRawMrEnclave } from "@switchboard-xyz/common";
 
 describe("ai-nft-generator", () => {
+  const unixTimestamp = () => Math.floor(Date.now() / 1000);
+  const MRENCLAVE = parseRawMrEnclave(
+    "0x363b16f752945498c0fafef9f14915141ffdd35729eb39d8dd330161552703dc"
+  );
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.AiNftGenerator as Program<AiNftGenerator>;
 
-  const payer = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.fromSecretKey(new Uint8Array(wallet));
+
+  const commitment: Commitment = "confirmed";
+  const connection = new Connection(
+    "https://api.devnet.solana.com",
+    commitment
+  );
 
   interface InitializeParams {}
   let initializeParams: InitializeParams = {};
 
+  const [programPda] = PublicKey.findProgramAddressSync(
+    [Buffer.from("ai-nft-generator")],
+    program.programId
+  );
+  const [oraclePda] = PublicKey.findProgramAddressSync(
+    [Buffer.from("ai-nft-generator-oracle")],
+    program.programId
+  );
+  let txId;
+  before(async () => {
+    try {
+      const switchboardProgram = await SwitchboardProgram.load(
+        "devnet",
+        connection,
+        payer
+      );
+      const [functionAccount, functionAccountData] = await FunctionAccount.load(
+        switchboardProgram,
+        "BvvrSwT1KFwXf8v4E7ZA1L7jdFTYzt1WcqBV8FfDtTJx"
+      );
+      /*let functionTriggerParams: FunctionTriggerParams = {
+        authority: payer,
+      };
+      txId = await functionAccount.trigger(functionTriggerParams);
+      console.log("txId", txId);*/
+      /*await AttestationProgramStateAccount.getOrCreate(switchboardProgram);
+      const switchboard = await AttestationQueueAccount.bootstrapNewQueue(
+        switchboardProgram
+      );
+      const securedSigner = anchor.web3.Keypair.generate();
+      const rewardAddress =
+        await switchboard.program.mint.getOrCreateAssociatedUser(
+          payer.publicKey
+        );
+      const functionVerifyIxn = attestationTypes.functionVerify(
+        switchboard.program,
+        {
+          params: {
+            observedTime: new anchor.BN(unixTimestamp()),
+            nextAllowedTimestamp: new anchor.BN(unixTimestamp() + 100),
+            isFailure: false,
+            mrEnclave: Array.from(MRENCLAVE),
+          },
+        },
+        {
+          function: functionAccount.publicKey,
+          functionEnclaveSigner: securedSigner.publicKey,
+          verifier: switchboard.verifier.publicKey,
+          verifierSigner: switchboard.verifier.signer.publicKey,
+          attestationQueue: switchboard.attestationQueue.publicKey,
+          escrowWallet: functionAccountData.escrowWallet,
+          escrowTokenWallet: functionAccountData.escrowTokenWallet,
+          receiver: rewardAddress,
+          verifierPermission: switchboard.verifier.permissionAccount.publicKey,
+          tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+        }
+      );*/
+      // const functionRequestAccountInitParams: FunctionRequestAccountInitParams =
+      //   {
+      //     functionAccount,
+      //     containerParams: Buffer.from("prompt"),
+      //     authority: payer.publicKey,
+      //   };
+      // const [functionRequestAccount, txId] =
+      //   await FunctionRequestAccount.create(
+      //     switchboardProgram,
+      //     functionRequestAccountInitParams
+      //   );
+      /*const [functionRequestAccount, functionRequestAccountData1] =
+        await FunctionRequestAccount.load(
+          switchboardProgram,
+          "BsNUF2vKEXqwFf49DyXDNd75UrYL69gGKxUYGq3uuddf"
+        );
+      // console.log("functionRequestAccountData1", functionRequestAccountData1);
+      const functionRequestAccountData =
+        await functionRequestAccount.loadData();
+      // console.log("functionRequestAccountData", functionRequestAccountData);
+      let functionRequestTriggerParams: FunctionRequestTriggerParams = {
+        authority: payer,
+      };
+      txId = await functionRequestAccount.trigger(functionRequestTriggerParams);
+      console.log("txId", txId);
+      const functionRequestSetConfigParams: FunctionRequestSetConfigParams = {
+        appendContainerParams: false,
+        containerParams: Buffer.from("prompt3"),
+        authority: payer,
+      };
+      txId = await functionRequestAccount.setConfig(
+        functionRequestSetConfigParams
+      );
+      console.log("txId", txId);*/
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods
-      .initialize(undefined)
-      .accounts({
-        oracle: payer.publicKey,
-      })
-      .rpc();
-    console.log("Your transaction signature", tx);
+    try {
+      console.log("programPda", programPda.toBase58());
+      const tx = await program.methods
+        .initialize({})
+        .accounts({
+          program: programPda,
+          oracle: oraclePda,
+          payer: payer.publicKey,
+          authority: payer.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([payer])
+        .rpc();
+      console.log("Your transaction signature", tx);
+    } catch (error) {
+      console.error(error);
+    }
   });
 });
--- tests/generate_matadata.ts
+++ tests/generate_matadata.ts
@@ -1,198 +1,198 @@
-// eslint-disable-next-line node/no-unpublished-import
-import type { AiNftGenerator } from "../target/types/ai_nft_generator";
+// // eslint-disable-next-line node/no-unpublished-import
+// import type { AiNftGenerator } from "../target/types/ai_nft_generator";
 
-import type { Program } from "@coral-xyz/anchor";
-import * as anchor from "@coral-xyz/anchor";
-import { parseRawMrEnclave, sleep } from "@switchboard-xyz/common";
-import type { FunctionAccount, MrEnclave } from "@switchboard-xyz/solana.js";
-import { SwitchboardWallet } from "@switchboard-xyz/solana.js";
-import {
-  AttestationProgramStateAccount,
-  AttestationQueueAccount,
-  attestationTypes,
-  type BootstrappedAttestationQueue,
-  SwitchboardProgram,
-  types,
-} from "@switchboard-xyz/solana.js";
+// import type { Program } from "@coral-xyz/anchor";
+// import * as anchor from "@coral-xyz/anchor";
+// import { parseRawMrEnclave, sleep } from "@switchboard-xyz/common";
+// import type { FunctionAccount, MrEnclave } from "@switchboard-xyz/solana.js";
+// import { SwitchboardWallet } from "@switchboard-xyz/solana.js";
+// import {
+//   AttestationProgramStateAccount,
+//   AttestationQueueAccount,
+//   attestationTypes,
+//   type BootstrappedAttestationQueue,
+//   SwitchboardProgram,
+//   types,
+// } from "@switchboard-xyz/solana.js";
 
-const unixTimestamp = () => Math.floor(Date.now() / 1000);
+// const unixTimestamp = () => Math.floor(Date.now() / 1000);
 
-// vv1gTnfuUiroqgJHS4xsRASsRQqqixCv1su85VWvcP9
+// // vv1gTnfuUiroqgJHS4xsRASsRQqqixCv1su85VWvcP9
 
-const MRENCLAVE = parseRawMrEnclave(
-    "0xec4e9cb9c6e78c0a008e35ea92e3a9478d9bfd14e15bbf88c93935f0951c1d98"
-    );
-const emptyEnclave: number[] = new Array(32).fill(0);
+// const MRENCLAVE = parseRawMrEnclave(
+//     "0xec4e9cb9c6e78c0a008e35ea92e3a9478d9bfd14e15bbf88c93935f0951c1d98"
+//     );
+// const emptyEnclave: number[] = new Array(32).fill(0);
 
-describe("ai-nft-generator", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+// describe("ai-nft-generator", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.AiNftGenerator as Program<AiNftGenerator>;
+//   const program = anchor.workspace.AiNftGenerator as Program<AiNftGenerator>;
 
-  console.log(`ProgramID: ${program.programId}`);
+//   console.log(`ProgramID: ${program.programId}`);
 
-  const payer = (program.provider as anchor.AnchorProvider).publicKey;
+//   const payer = (program.provider as anchor.AnchorProvider).publicKey;
 
-  const programStatePubkey = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("ai-nft-generator")],
-    program.programId
-  )[0];
+//   const programStatePubkey = anchor.web3.PublicKey.findProgramAddressSync(
+//     [Buffer.from("ai-nft-generator")],
+//     program.programId
+//   )[0];
 
-  const oraclePubkey = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("ai-nft-generator-oracle")],
-    program.programId
-  )[0];
+//   const oraclePubkey = anchor.web3.PublicKey.findProgramAddressSync(
+//     [Buffer.from("ai-nft-generator-oracle")],
+//     program.programId
+//   )[0];
 
-  let switchboard: BootstrappedAttestationQueue;
-  let wallet: SwitchboardWallet;
-  let functionAccount: FunctionAccount;
+//   let switchboard: BootstrappedAttestationQueue;
+//   let wallet: SwitchboardWallet;
+//   let functionAccount: FunctionAccount;
 
-  before(async () => {
-    const switchboardProgram = await SwitchboardProgram.fromProvider(
-      program.provider as anchor.AnchorProvider
-    );
+//   before(async () => {
+//     const switchboardProgram = await SwitchboardProgram.fromProvider(
+//       program.provider as anchor.AnchorProvider
+//     );
 
-    await AttestationProgramStateAccount.getOrCreate(switchboardProgram);
+//     await AttestationProgramStateAccount.getOrCreate(switchboardProgram);
 
-    switchboard = await AttestationQueueAccount.bootstrapNewQueue(
-      switchboardProgram
-    );
+//     switchboard = await AttestationQueueAccount.bootstrapNewQueue(
+//       switchboardProgram
+//     );
 
-    console.log(`programStatePubkey: ${programStatePubkey}`);
+//     console.log(`programStatePubkey: ${programStatePubkey}`);
 
-    [wallet] = await SwitchboardWallet.create(
-      switchboard.program,
-      switchboard.attestationQueue.publicKey,
-      payer,
-      "MySharedWallet",
-      16
-    );
+//     [wallet] = await SwitchboardWallet.create(
+//       switchboard.program,
+//       switchboard.attestationQueue.publicKey,
+//       payer,
+//       "MySharedWallet",
+//       16
+//     );
 
-    console.log(`wallet: ${wallet.publicKey}`);
+//     console.log(`wallet: ${wallet.publicKey}`);
 
-    [functionAccount] =
-      await switchboard.attestationQueue.account.createFunction(
-        {
-          name: "test function",
-          metadata: "this function handles XYZ for my protocol",
-          schedule: "15 * * * * *",
-          container: "switchboardlabs/basic-oracle-function",
-          version: "latest",
-          mrEnclave: MRENCLAVE,
-          authority: programStatePubkey,
-        },
-        wallet
-      );
+//     [functionAccount] =
+//       await switchboard.attestationQueue.account.createFunction(
+//         {
+//           name: "test function",
+//           metadata: "this function handles XYZ for my protocol",
+//           schedule: "15 * * * * *",
+//           container: "switchboardlabs/basic-oracle-function",
+//           version: "latest",
+//           mrEnclave: MRENCLAVE,
+//           authority: programStatePubkey,
+//         },
+//         wallet
+//       );
 
-    console.log(`functionAccount: ${functionAccount.publicKey}`);
-  });
+//     console.log(`functionAccount: ${functionAccount.publicKey}`);
+//   });
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods
-      .initialize({})
-      .accounts({
-        program: programStatePubkey,
-        oracle: oraclePubkey,
-        authority: payer,
-        payer: payer,
-        // function: functionAccount.publicKey,
-      })
-      .rpc()
-      .catch((err) => {
-        console.error(err);
-        throw err;
-      });
-    console.log("Your transaction signature", tx);
-  });
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const tx = await program.methods
+//       .initialize({})
+//       .accounts({
+//         program: programStatePubkey,
+//         oracle: oraclePubkey,
+//         authority: payer,
+//         payer: payer,
+//         // function: functionAccount.publicKey,
+//       })
+//       .rpc()
+//       .catch((err) => {
+//         console.error(err);
+//         throw err;
+//       });
+//     console.log("Your transaction signature", tx);
+//   });
 
-  // it("Adds an enclave measurement", async () => {
-  //   // Add your test here.
-  //   const tx = await program.methods
-  //     .setEnclaves({ mrEnclaves: [Array.from(MRENCLAVE)] })
-  //     .accounts({
-  //       program: programStatePubkey,
-  //       authority: payer,
-  //     })
-  //     .rpc()
-  //     .catch((err) => {
-  //       console.error(err);
-  //       throw err;
-  //     });
-  //   console.log("Your transaction signature", tx);
-  //   const programState = await program.account.myProgramState.fetch(
-  //     programStatePubkey
-  //   );
-  // });
+//   // it("Adds an enclave measurement", async () => {
+//   //   // Add your test here.
+//   //   const tx = await program.methods
+//   //     .setEnclaves({ mrEnclaves: [Array.from(MRENCLAVE)] })
+//   //     .accounts({
+//   //       program: programStatePubkey,
+//   //       authority: payer,
+//   //     })
+//   //     .rpc()
+//   //     .catch((err) => {
+//   //       console.error(err);
+//   //       throw err;
+//   //     });
+//   //   console.log("Your transaction signature", tx);
+//   //   const programState = await program.account.myProgramState.fetch(
+//   //     programStatePubkey
+//   //   );
+//   // });
 
-  it("generate metadata", async () => {
-    const securedSigner = anchor.web3.Keypair.generate();
+//   it("generate metadata", async () => {
+//     const securedSigner = anchor.web3.Keypair.generate();
 
-    const rewardAddress =
-      await switchboard.program.mint.getOrCreateAssociatedUser(payer);
+//     const rewardAddress =
+//       await switchboard.program.mint.getOrCreateAssociatedUser(payer);
 
-    const functionState = await functionAccount.loadData();
+//     const functionState = await functionAccount.loadData();
 
-    // TODO: generate function verify ixn
-    const functionVerifyIxn = attestationTypes.functionVerify(
-      switchboard.program,
-      {
-        params: {
-          observedTime: new anchor.BN(unixTimestamp()),
-          nextAllowedTimestamp: new anchor.BN(unixTimestamp() + 100),
-          isFailure: false,
-          mrEnclave: Array.from(MRENCLAVE),
-        },
-      },
-      {
-        function: functionAccount.publicKey,
-        functionEnclaveSigner: securedSigner.publicKey,
-        verifier: switchboard.verifier.publicKey,
-        verifierSigner: switchboard.verifier.signer.publicKey,
-        attestationQueue: switchboard.attestationQueue.publicKey,
-        escrowWallet: functionState.escrowWallet,
-        escrowTokenWallet: functionState.escrowTokenWallet,
-        receiver: rewardAddress,
-        verifierPermission: switchboard.verifier.permissionAccount.publicKey,
-        tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-      }
-    );
+//     // TODO: generate function verify ixn
+//     const functionVerifyIxn = attestationTypes.functionVerify(
+//       switchboard.program,
+//       {
+//         params: {
+//           observedTime: new anchor.BN(unixTimestamp()),
+//           nextAllowedTimestamp: new anchor.BN(unixTimestamp() + 100),
+//           isFailure: false,
+//           mrEnclave: Array.from(MRENCLAVE),
+//         },
+//       },
+//       {
+//         function: functionAccount.publicKey,
+//         functionEnclaveSigner: securedSigner.publicKey,
+//         verifier: switchboard.verifier.publicKey,
+//         verifierSigner: switchboard.verifier.signer.publicKey,
+//         attestationQueue: switchboard.attestationQueue.publicKey,
+//         escrowWallet: functionState.escrowWallet,
+//         escrowTokenWallet: functionState.escrowTokenWallet,
+//         receiver: rewardAddress,
+//         verifierPermission: switchboard.verifier.permissionAccount.publicKey,
+//         tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+//       }
+//     );
 
-    // generate nft metadata
-    const tx = await program.methods
-      .mintNft({
-        {
-            "name": "My NFT",
-            "description": "My NFT Description",
-            "image": "https://gateway.pinata.cloud/ipfs/QmZJk9tQh8J5Z6E6iXhV6QZ4fY4G8mN2Z5mZ5Qy3V3XrZ1",
-            "external_url": "https://gateway.pinata.cloud/ipfs/QmZJk9tQh8J5Z6E6iXhV6QZ4fY4G8mN2Z5mZ5Qy3V3XrZ1",
-        }
-      })
-      .accounts({
-        oracle: oraclePubkey,
-        function: functionAccount.publicKey,
-        enclaveSigner: securedSigner.publicKey,
-      })
-      .preInstructions([functionVerifyIxn])
-      .signers([switchboard.verifier.signer, securedSigner])
-      .rpc({ skipPreflight: true });
+//     // generate nft metadata
+//     const tx = await program.methods
+//       .mintNft({
+//         {
+//             "name": "My NFT",
+//             "description": "My NFT Description",
+//             "image": "https://gateway.pinata.cloud/ipfs/QmZJk9tQh8J5Z6E6iXhV6QZ4fY4G8mN2Z5mZ5Qy3V3XrZ1",
+//             "external_url": "https://gateway.pinata.cloud/ipfs/QmZJk9tQh8J5Z6E6iXhV6QZ4fY4G8mN2Z5mZ5Qy3V3XrZ1",
+//         }
+//       })
+//       .accounts({
+//         oracle: oraclePubkey,
+//         function: functionAccount.publicKey,
+//         enclaveSigner: securedSigner.publicKey,
+//       })
+//       .preInstructions([functionVerifyIxn])
+//       .signers([switchboard.verifier.signer, securedSigner])
+//       .rpc({ skipPreflight: true });
 
-    console.log("Your transaction signature", tx);
+//     console.log("Your transaction signature", tx);
 
-    await printLogs(switchboard.program.connection, tx ? tx : "");
+//     await printLogs(switchboard.program.connection, tx ? tx : "");
 
-    await sleep(5000);
+//     await sleep(5000);
 
-    const oracleState = await program.account.myOracleState.fetch(oraclePubkey);
+//     const oracleState = await program.account.myOracleState.fetch(oraclePubkey);
 
-    console.log(oracleState);
+//     console.log(oracleState);
 
-  });
-});
+//   });
+// });
 
-function normalizeDecimals(value: anchor.BN) {
-  return (value ?? new anchor.BN(0))
-    .div(new anchor.BN(10).pow(new anchor.BN(9)))
-    .toNumber();
-}
+// function normalizeDecimals(value: anchor.BN) {
+//   return (value ?? new anchor.BN(0))
+//     .div(new anchor.BN(10).pow(new anchor.BN(9)))
+//     .toNumber();
+// }
