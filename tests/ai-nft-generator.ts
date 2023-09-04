6b827d790d649e2743a03d09d08529c1307b0102
raghav-rama <hackerer528@gmail.com>
2023-09-04 15:49:08 +0530

    feat: add tests for new ixns

--- tests/ai-nft-generator.ts
+++ tests/ai-nft-generator.ts
@@ -18,6 +18,8 @@ import {
   AttestationProgramStateAccount,
   BootstrappedAttestationQueue,
   SwitchboardWallet,
+  getSwitchboardAttestationProgramId,
+  NativeMint,
 } from "@switchboard-xyz/solana.js";
 import { parseRawMrEnclave } from "@switchboard-xyz/common";
 import { DataV2 } from "@metaplex-foundation/mpl-token-metadata";
@@ -35,7 +37,11 @@ describe("ai-nft-generator", () => {
   const payer = anchor.web3.Keypair.fromSecretKey(new Uint8Array(wallet));
 
   const commitment: Commitment = "confirmed";
-  const connection = new Connection("http://localhost:8899", commitment);
+  const connection = new Connection(
+    // "https://api.devnet.solana.com",
+    "http://localhost:8899",
+    commitment
+  );
 
   interface InitializeParams {}
   let initializeParams: InitializeParams = {};
@@ -49,9 +55,15 @@ describe("ai-nft-generator", () => {
     program.programId
   );
   let txId;
+  let functionVerifyIxn: anchor.web3.TransactionInstruction;
+  let functionAccountData: attestationTypes.FunctionAccountData;
+  let functionAccountG: FunctionAccount;
+  let switchboardG: BootstrappedAttestationQueue;
+  let requestAccountKeypairG: anchor.web3.Keypair;
   before(async () => {
     try {
       const switchboardProgram = await SwitchboardProgram.load(
+        // "devnet",
         "localnet",
         connection,
         payer
@@ -66,6 +78,7 @@ describe("ai-nft-generator", () => {
         "MySharedWallet",
         16
       );
+      switchboardG = switchboard;
       const functionAccountInitParams: FunctionAccountInitParams = {
         name: "test function",
         metadata: "test metadata",
@@ -76,18 +89,19 @@ describe("ai-nft-generator", () => {
         authority: payer.publicKey,
         attestationQueue: switchboard.attestationQueue.account,
       };
-      let [functionAccount, txId] =
+      const [functionAccount, txId2] =
         await switchboard.attestationQueue.account.createFunction(
           functionAccountInitParams,
           wallet
         );
+      functionAccountG = functionAccount;
       console.log("functionAccount", functionAccount.publicKey.toBase58());
-      console.log("txId", txId);
+      console.log("txId2", txId2);
       let functionTriggerParams: FunctionTriggerParams = {
         authority: payer,
       };
-      txId = await functionAccount.trigger(functionTriggerParams);
-      console.log("txId", txId);
+      const txId3 = await functionAccount.trigger(functionTriggerParams);
+      console.log("txId3", txId3);
       // const [functionAccount, functionAccountData] = await FunctionAccount.load(
       //   switchboardProgram,
       //   "BvvrSwT1KFwXf8v4E7ZA1L7jdFTYzt1WcqBV8FfDtTJx"
@@ -95,49 +109,28 @@ describe("ai-nft-generator", () => {
       /*
       txId = await functionAccount.trigger(functionTriggerParams);
       console.log("txId", txId);*/
-      /*await AttestationProgramStateAccount.getOrCreate(switchboardProgram);
-      const switchboard = await AttestationQueueAccount.bootstrapNewQueue(
-        switchboardProgram
-      );
-      const securedSigner = anchor.web3.Keypair.generate();
-      const rewardAddress =
-        await switchboard.program.mint.getOrCreateAssociatedUser(
-          payer.publicKey
+      functionAccountData = await functionAccount.loadData();
+      await AttestationProgramStateAccount.getOrCreate(switchboardProgram);
+      // const switchboard = await AttestationQueueAccount.bootstrapNewQueue(
+      //   switchboardProgram
+      // );
+
+      const functionRequestAccountInitParams: FunctionRequestAccountInitParams =
+        {
+          functionAccount,
+          containerParams: Buffer.from("cats in space"),
+          authority: payer.publicKey,
+        };
+      const [functionRequestAccount, txId4] =
+        await FunctionRequestAccount.create(
+          switchboardProgram,
+          functionRequestAccountInitParams
         );
-      const functionVerifyIxn = attestationTypes.functionVerify(
-        switchboard.program,
-        {
-          params: {
-            observedTime: new anchor.BN(unixTimestamp()),
-            nextAllowedTimestamp: new anchor.BN(unixTimestamp() + 100),
-            isFailure: false,
-            mrEnclave: Array.from(MRENCLAVE),
-          },
-        },
-        {
-          function: functionAccount.publicKey,
-          functionEnclaveSigner: securedSigner.publicKey,
-          verifier: switchboard.verifier.publicKey,
-          verifierSigner: switchboard.verifier.signer.publicKey,
-          attestationQueue: switchboard.attestationQueue.publicKey,
-          escrowWallet: functionAccountData.escrowWallet,
-          escrowTokenWallet: functionAccountData.escrowTokenWallet,
-          receiver: rewardAddress,
-          verifierPermission: switchboard.verifier.permissionAccount.publicKey,
-          tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-        }
-      );*/
-      // const functionRequestAccountInitParams: FunctionRequestAccountInitParams =
-      //   {
-      //     functionAccount,
-      //     containerParams: Buffer.from("prompt"),
-      //     authority: payer.publicKey,
-      //   };
-      // const [functionRequestAccount, txId] =
-      //   await FunctionRequestAccount.create(
-      //     switchboardProgram,
-      //     functionRequestAccountInitParams
-      //   );
+      console.log(
+        "functionRequestAccount",
+        functionRequestAccount.publicKey.toBase58()
+      );
+      console.log("txId4", txId4);
       /*const [functionRequestAccount, functionRequestAccountData1] =
         await FunctionRequestAccount.load(
           switchboardProgram,
@@ -186,9 +179,135 @@ describe("ai-nft-generator", () => {
       console.error(error);
     }
   });
-  it("fetches nft metadata", async () => {
+  it("sets function", async () => {
     try {
-      // const
+      const tx = await program.methods
+        .setFunction({})
+        .accounts({
+          function: functionAccountG.publicKey,
+          program: programPda,
+          authority: payer.publicKey,
+        })
+        .signers([payer])
+        .rpc();
+      console.log("Your transaction signature", tx);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  it("sets the user prompt", async () => {
+    try {
+      const requestAccountKeypair = anchor.web3.Keypair.generate();
+      requestAccountKeypairG = requestAccountKeypair;
+      console.log(
+        `requestAccountKeypair`,
+        requestAccountKeypair.publicKey.toBase58()
+      );
+      const requestAccount = new FunctionRequestAccount(
+        switchboardG.program,
+        requestAccountKeypair.publicKey
+      );
+      const functionState = await functionAccountG.loadData();
+
+      const tx = await program.methods
+        .setUserPrompt({
+          prompt: "cats in space",
+        })
+        .accounts({
+          program: programPda,
+          switchboard: switchboardG.program.attestationProgramId,
+          state: switchboardG.program.attestationProgramState.publicKey,
+          attestationQueue: switchboardG.attestationQueue.publicKey,
+          function: functionAccountG.publicKey,
+          requestAccount: requestAccountKeypair.publicKey,
+          requestAccountEscrow: anchor.utils.token.associatedAddress({
+            mint: NativeMint.address,
+            owner: requestAccountKeypair.publicKey,
+          }),
+          mint: NativeMint.address,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          payer: payer.publicKey,
+        })
+        .signers([requestAccountKeypair, payer])
+        .rpc();
+      console.log("Your transaction signature", tx);
+      const requestState = await requestAccount.loadData();
+      console.log("requestState", requestState.containerParams);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  it("triggers the function", async () => {
+    try {
+      const tx = await program.methods
+        .triggerFunction({})
+        .accounts({
+          function: functionAccountG.publicKey,
+          requestAccount: requestAccountKeypairG.publicKey,
+          attestationQueue: switchboardG.attestationQueue.publicKey,
+          authority: payer.publicKey,
+          atestationProgram: getSwitchboardAttestationProgramId("devnet"),
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([requestAccountKeypairG, payer])
+        .rpc();
+      console.log("Your transaction signature", tx);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  it("generates the nft", async () => {
+    const enclaveSigner = anchor.web3.Keypair.generate();
+    console.log(`enclaveSigner`, enclaveSigner.publicKey.toBase58());
+    const rewardAddress =
+      await switchboardG.program.mint.getOrCreateAssociatedUser(
+        payer.publicKey
+      );
+    functionVerifyIxn = attestationTypes.functionVerify(
+      switchboardG.program,
+      {
+        params: {
+          observedTime: new anchor.BN(unixTimestamp()),
+          nextAllowedTimestamp: new anchor.BN(unixTimestamp() + 100),
+          isFailure: false,
+          mrEnclave: Array.from(MRENCLAVE),
+        },
+      },
+      {
+        function: functionAccountG.publicKey,
+        functionEnclaveSigner: enclaveSigner.publicKey,
+        verifier: switchboardG.verifier.publicKey,
+        verifierSigner: switchboardG.verifier.signer.publicKey,
+        attestationQueue: switchboardG.attestationQueue.publicKey,
+        escrowWallet: functionAccountData.escrowWallet,
+        escrowTokenWallet: functionAccountData.escrowTokenWallet,
+        receiver: rewardAddress,
+        verifierPermission: switchboardG.verifier.permissionAccount.publicKey,
+        tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      }
+    );
+    try {
+      const tx = await program.methods
+        .mintAiNft({
+          nft: {
+            name: [1],
+            symbol: [1],
+            description: [1],
+            image: [1],
+            animationUrl: [1],
+            externalUrl: [1],
+          },
+        })
+        .accounts({
+          function: functionAccountG.publicKey,
+          oracle: oraclePda,
+          enclaveSigner: enclaveSigner.publicKey,
+          request: requestAccountKeypairG.publicKey,
+        })
+        .preInstructions([functionVerifyIxn])
+        .signers([switchboardG.verifier.signer, enclaveSigner])
+        .rpc();
+      console.log("Your transaction signature", tx);
     } catch (error) {
       console.log(error);
     }
